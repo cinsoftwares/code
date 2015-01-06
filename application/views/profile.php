601f2cae42cc5665e0641e6bf97a8cbde76a580d
CinSoftwares <cin.softwares@hotmail.com>
2015-01-06 02:26:14 -0500

    add profile details

--- application/views/profile.php
+++ application/views/profile.php
@@ -4,6 +4,21 @@
 		
 	</head>
 	<body>
+		<center>
+			<font color="blue">
 		<h1>Profile Details</h1>
+		<h2>Name-Aiswarya b unnithan</h2>
+		<h2>Education Qualification-BSC CS</h2>
+		<h2>Email id-sangeethasunnithanevk@gmail.com</h2>
+		</center>
+	</font>
+	<center>
+		<font color="red">
+		<h2>Name-Ani Rajan</h2>
+		<h2>Education Qualification-BSC CS</h2>
+		<h2>Email id-anirajan@gmail.com</h2>
+		</center>
+			
+		</font>
 	</body>
 </html>
